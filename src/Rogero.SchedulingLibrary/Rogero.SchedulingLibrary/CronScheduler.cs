6f45cad788f0fb45522bc4247ef32bbcf8ed8b14
Paul Rogero <paulrogero@gmail.com>
2016-03-21 18:27:56 -0400

    Added missing files.

---
+++ src/Rogero.SchedulingLibrary/Rogero.SchedulingLibrary/CronScheduler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rogero.SchedulingLibrary
+{
+    public interface IDateTimeRepository
+    {
+        DateTime Now();
+    }
+
+    public class CronSchedulerCallback
+    {
+        private readonly IDateTimeRepository _dateTimeRepository;
+        private readonly CronTemplate _cronTemplate;
+        private Action _callBack;
+        private CronTime _cronTime;
+
+        public CronSchedulerCallback(IDateTimeRepository dateTimeRepository, CronTemplate cronTemplate)
+        {
+            _dateTimeRepository = dateTimeRepository;
+            _cronTemplate = cronTemplate;
+            CreateCronTimeQueue();
+        }
+
+        public void Start(Action callback)
+        {
+            _callBack = callback;
+            Initialize();
+            Start();
+        }
+
+        private void Initialize()
+        {
+            CreateCronTimeQueue();
+        }
+
+        private void CreateCronTimeQueue()
+        {
+            _cronTime = new CronTime(_cronTemplate, _dateTimeRepository.Now());
+            var cronTimes = CronTimeGenerator.GenerateForLessorOf(_cronTime, TimeSpan.FromMinutes(5), 100);
+        }
+
+        private void Start()
+        {
+            
+        }
+    }
+}
---
+++ src/Rogero.SchedulingLibrary/Rogero.SchedulingLibrary/ListExtensionMethods2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rogero.SchedulingLibrary
+{
+    public static class ListExtensionMethods2
+    {
+        public static void AddRange<T>(this IList<T> list, IEnumerable<T> newItems)
+        {
+            foreach (var newItem in newItems)
+            {
+                list.Add(newItem);
+            }
+        }
+    }
+}
