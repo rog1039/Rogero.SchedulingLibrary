0f4fd69f5b96486b5559e1bc97b226ca6be2a303
Paul Rogero <paulrogero@gmail.com>
2016-03-31 10:52:29 -0400

    Added missing file.

---
+++ src/Rogero.SchedulingLibrary/Rogero.SchedulingLibrary.Portable/Scheduling/SchedulerReactive.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Reactive.Concurrency;
+using System.Reactive.Subjects;
+using Rogero.SchedulingLibrary.Streams;
+
+namespace Rogero.SchedulingLibrary.Scheduling
+{
+    public class SchedulerReactive
+    {
+        public IObservable<CronTime> SchedulerObservable => GetEventStream();
+        public IEnumerable<CronTime> UpcomingEvents => _scheduler.UpcomingEvents;
+        public CronTime LastFiredSchedule => _scheduler.LastFiredEvent;
+
+        private readonly Scheduler _scheduler;
+        private bool _schedulerStarted = false;
+        private readonly Subject<CronTime> _schedulerCallbackObservable = new Subject<CronTime>();
+
+        public SchedulerReactive(IDateTimeRepository dateTimeRepository, IScheduler scheduler, CronTimeStreamBase cronTimeStream)
+        {
+            _scheduler = new Scheduler(dateTimeRepository, scheduler, cronTimeStream);
+        }
+
+        private IObservable<CronTime> GetEventStream()
+        {
+            if (!_schedulerStarted) _scheduler.Start((cronTime) => _schedulerCallbackObservable.OnNext(cronTime));
+            _schedulerStarted = true;
+            return _schedulerCallbackObservable;
+        }
+    }
+}
